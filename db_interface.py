634400fd23b929ca7709e0c2ba1269b5765643c7
Sandeep Bhatia <sabhatia@gmail.com>
2024-02-18 19:37:53 -0800

    Refactored to put DB operations into its own file

--- db_interface.py
+++ db_interface.py
@@ -4,6 +4,8 @@ import argparse
 import csv
 from pprint import pprint as pp
 from memory_db import memory_db
+from memory_db import DB_Initialization_Error
+
 
 def build_parser():
 
@@ -13,81 +15,48 @@ def build_parser():
     parser.add_argument('filename', help="filename to load")
     return parser
 
-def open_csv_file(file_path_name):
-    """
-    Function to open a file
-
-    Input
-        Arg: the name of the file to use
-
-    Output
-        Status: If the operation succeeded or failed
-        file_pointer: pointer to the opened file
-    
-    """
-    file_pointer = -1
-    print(f"STATUS: Loading file {file_path_name}")
-    mode = 'rw'
-    try:
-        file_pointer = open(file_path_name) 
-        return True, file_pointer
-    except FileNotFoundError:
-        return False, file_pointer
-
-def load_csv_file(csv_file_pointer):
-    data = []
-    lines = csv_file_pointer.readlines()
-    header_row = lines[0].strip().split(',')
-    for line in lines[1:]:
-        row = line.strip().split(',')
-        data.append(row)
-
-    pp(header_row)
-    pp(data[1:6])
-    return True, memory_db(header_row, data)
-
-def end_display(mem_db):
-    print("%%STATUS%%: Exit")
-    return END
-
 def display_records(mem_db):
     record_count = mem_db.total_records_db()
-    print("%%STATUS%%: Display Records")
+    print("[STATUS]: : Display Records")
     print("Total Records: ", record_count)
     first_record = int(input("Enter starting range: [0 for all]:"))
     last_record = int(input("Enter last record: "))
 
     mem_db.display_db(first_record, last_record)
     if (first_record > record_count or last_record > record_count):
-        print(f"%%ERROR%%: Records should be between 1 - {record_count}")
+        print(f"[ERROR]: : Records should be between 1 - {record_count}")
 
     return True
 
 def delete_records(mem_db):
-    print("%%STATUS%%: Delete Records")
+    print("[STATUS]: : Delete Records")
     return True
 
 def update_records(mem_db):
-    print("%%STATUS%%: Update Records")
+    print("[STATUS]: : Update Records")
     return True
+
+def end_display(mem_db):
+    print("[STATUS]: : Exit")
+    return END
+
+user_cmds_dict = {'0': end_display,
+                  '1': display_records,
+                  '2': delete_records,
+                  '3': update_records}
 
 def display_and_parse_console(mem_db):
     ret_val = True
-    user_cmds_dict = {'0': end_display,
-                      '1': display_records,
-                      '2': delete_records,
-                      '3': update_records}
     print('File Operations')
     print('[1] Display records')
     print('[2] Delete records')
     print('[3] Update records')
     print('[0] Exit')
     user_cmd = input("Your Selection: ")
-
     if user_cmd in user_cmds_dict.keys():
         ret_val = user_cmds_dict[user_cmd](mem_db)
     else:
-        print(f"%%ERROR%%: Invalid entry {user_cmd}. Accepted range 0-3")
+        print(f"[ERROR]: Invalid entry {user_cmd}. Accepted range 0-3")
     return ret_val
 
 if __name__ == '__main__':
@@ -102,21 +71,16 @@ if __name__ == '__main__':
     args = cli_parser.parse_args()
     csv_file = args.filename
     
-    # 3. Ensure file is useable
-    file_load_status, file_pointer = open_csv_file(csv_file)
-    if file_load_status == False:
-        print("%%ERROR%%: Unable to find/load file:", csv_file)
-        exit()
-
-    # 4. Read to memory
-    file_page_status, csv_table = load_csv_file(file_pointer)
-    if file_page_status == False:
-        print("%%ERROR%%: Couldn't load file into memory")
+    # 3. Initialize the DB
+    try:
+        csv_table = memory_db(csv_file)
+    except DB_Initialization_Error as dbE:
+        print("[ERROR]: ", dbE.message)
         exit()
 
     # 4. Diplay the console for further instructions
     while (display_and_parse_console(csv_table) != END):
         continue
 
-    # Done, close the file
-    file_pointer.close()
+    # Done, shutdown db
+    del csv_table
--- memory_db.py
+++ memory_db.py
@@ -3,12 +3,58 @@ This file defines a class that performs CRUD operations.
 These operations are done on an in memory DB.
 It also supports load and flush operations to a file on disk. 
 '''
-from pprint import pprint
+from pprint import pprint as pp
 
 class memory_db:
-    def __init__(self, header, data):
-        self.header = header
-        self.data = data
+    def __init__(self, csv_filename: str):
+
+        self.csv_filename = csv_filename     
+        self.csv_file_pointer = -1
+        try:   
+            self.csv_file_pointer= open(self.csv_filename, mode='r') 
+        except FileNotFoundError as fe:
+            raise DB_Initialization_Error(csv_filename, 
+                                          "Could not open file: " + csv_filename)
+        
+        self.header, self.data = memory_db.load_csv_file(self.csv_file_pointer)
+
+
+    def __del__(self):
+        if self.csv_file_pointer == -1:
+            return
+        self.csv_file_pointer.close()
+
+    @staticmethod
+    def open_csv_file(file_path_name: str):
+        """
+        Function to open a file
+
+        Input
+            Arg: the name of the file to use
+
+        Output
+            Status: If the operation succeeded or failed
+            file_pointer: pointer to the opened file
+        
+        """
+        file_pointer = -1
+        print(f"[STATUS]: : Loading file {file_path_name}")
+        mode = 'rw'
+        return open()
+
+    @staticmethod    
+    def load_csv_file(csv_file_pointer):
+        data = []
+        lines = csv_file_pointer.readlines()
+        header_row = lines[0].strip().split(',')
+        for line in lines[1:]:
+            row = line.strip().split(',')
+            data.append(row)
+
+        pp(header_row)
+        pp(data[1:6])
+        
+        return header_row, data
 
     def display_db(self, first_row = 0, last_row = 5):
         print(self.header)
@@ -21,3 +67,9 @@ class memory_db:
     
     def total_records_db(self):
         return len(self.data)
+    
+class DB_Initialization_Error(Exception):
+    def __init__(self, file_name: str, message: str):
+        super().__init__()
+        self.file_name = file_name
+        self.message = message
